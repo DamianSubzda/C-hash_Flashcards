f71b0a366acfe7e7fe08cbe33d24d3e6e7cea4da
damia <DamianSubzda@wp.pl>
2022-01-19 18:45:07 +0100

    Dodaj pliki projektów.

---
+++ Projekt-fiszki/DataBaseConnector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class DataBaseConnector
+{
+
+	public static string token = "Data Source=DESKTOP-GH9TGES\\SQLEXPRESS;Initial Catalog=Flashcard;Integrated Security=True";
+	SqlConnection con = new SqlConnection(token);
+
+	public DataBaseConnector()
+	{
+		
+	}
+
+	public void Connection()
+    {
+		con.Open();
+
+		if(con.State == System.Data.ConnectionState.Open)
+        {
+			Console.WriteLine("It works");
+        }
+        else
+        {
+			Console.WriteLine("nie nie dziala");
+        }
+
+		con.Close();
+	}
+
+	public void GetRecordFromDB()
+	{
+
+	}
+
+}
