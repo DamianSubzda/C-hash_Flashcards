156c80390a4d62462880bb82fe162ff2e4cb250b
damia <DamianSubzda@wp.pl>
2022-01-21 10:26:27 +0100

    n

--- Projekt-fiszki/Remember.cs
+++ Projekt-fiszki/Remember.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 
 public class Remeber
 {
-    public List<String> firstWord = new List<string>();
-    public List<String> secondWord = new List<string>();
+    public List<String> rememberElement = new List<string>();
     public Remeber()
     {
     }
-    public void AddWords(String one, String two)
+    public void AddWord(String temp)
     {
-        firstWord.Add(one);
-        secondWord.Add(two);
+        rememberElement.Add(temp);
     }
+    /*
     public List<String> getFirstWord()
     {
         return firstWord;
@@ -21,5 +20,58 @@ public class Remeber
     {
         return secondWord;
     }
+    */
+    public String parseString(int firstLanguage, int secondLanguage, String firstWord, String secondWord)
+    {
+        String temp = "";
+        switch (firstLanguage)
+        {
+            case 1:
+                temp = "Polski: \t";
+                break;
+            case 2:
+                temp = "Angielski: \t";
+                break;
+            case 3:
+                temp = "Francuski: \t";
+                break;
+            case 4:
+                temp = "Włoski: \t";
+                break;
+        }
 
+        temp += firstWord;
+
+        switch (secondLanguage)
+        {
+            case 1:
+                temp += "\t\t Polski: \t";
+                break;
+            case 2:
+                temp += "\t\t Angielski: \t";
+                break;
+            case 3:
+                temp += "\t\t Francuski: \t";
+                break;
+            case 4:
+                temp += "\t\t Włoski: \t";
+                break;
+        }
+
+        temp += secondWord;
+        
+        return temp;
+    }
+    public Boolean checkList(String temp)
+    {
+        //if(jest w liście to){
+        //    return false;
+        //}
+        //else
+        //{
+        //    AddWord(temp);
+        //    return true;
+        //}
+        return false; //albo true
+    }
 }
